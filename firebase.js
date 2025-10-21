567e2c01a68ae824abdeb7543a4e3828617ce064
baeliozdays <annette.l.ma@vanderbilt.edu>
2025-10-21 10:09:58 -0500

    contributors

--- firebase.js
+++ firebase.js
@@ -1,3 +1,8 @@
+/*
+Contributors:
+Annette Ma - initialisation - 30 minutes
+*/
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
